e97bc6fd3fdb61bf03e34b7cbaf60582fb4a2d93
kirlozavr <kirill.protchenko@mail.ru>
2023-04-16 21:21:10 +1000

    Сущность ChangeSubscriberTariff

---
+++ src/main/java/com/application/billingsystem/entity/ChangeSubscriberTariffEntity.java
@@ -0,0 +1,46 @@
+package com.application.billingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "change_subscriber_tariff")
+public class ChangeSubscriberTariffEntity {
+
+    @Id
+    @SequenceGenerator(
+            name = "change_subscriber_tariff_id_seq",
+            sequenceName = "change_subscriber_tariff_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "change_subscriber_tariff_id_seq"
+    )
+    private long id;
+    private String numberPhone;
+    private String tariffIndex;
+
+    public ChangeSubscriberTariffEntity(String numberPhone, String tariffIndex) {
+        this.numberPhone = numberPhone;
+        this.tariffIndex = tariffIndex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChangeSubscriberTariffEntity that)) return false;
+        return id == that.id && numberPhone.equals(that.numberPhone) && tariffIndex.equals(that.tariffIndex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, numberPhone, tariffIndex);
+    }
+}
