9f459061333ded4a6d6af4201dc32e2b26e26457
kirlozavr <kirill.protchenko@mail.ru>
2023-04-16 20:25:34 +1000

    Сущность звонка

--- src/main/java/com/application/billingsystem/entity/PayloadEntity.java
+++ src/main/java/com/application/billingsystem/entity/PayloadEntity.java
@@ -1,14 +1,52 @@
 package com.application.billingsystem.entity;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "payload")
 public class PayloadEntity {
 
-    private final long id;
-    private final String callType;
-    private final String startTime;
-    private final String endTime;
-    private final String duration;
-    private final float cost;
+    @Id
+    @SequenceGenerator(
+            name = "tariff_id_seq",
+            sequenceName = "tariff_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "tariff_id_seq"
+    )
+    private long id;
+    private String callType;
+    private String startTime;
+    private String endTime;
+    private String duration;
+    private float cost;
+
+    public PayloadEntity(String callType, String startTime, String endTime, String duration, float cost) {
+        this.callType = callType;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.duration = duration;
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PayloadEntity that)) return false;
+        return id == that.id && Float.compare(that.cost, cost) == 0 && callType.equals(that.callType) && startTime.equals(that.startTime) && endTime.equals(that.endTime) && duration.equals(that.duration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, callType, startTime, endTime, duration, cost);
+    }
 }
