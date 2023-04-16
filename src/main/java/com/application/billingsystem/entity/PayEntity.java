793b14e276f806bc01c33bfa2f8d8fcd4ebfa4f7
kirlozavr <kirill.protchenko@mail.ru>
2023-04-16 21:43:50 +1000

    Сущность оплаты

--- src/main/java/com/application/billingsystem/entity/PayEntity.java
+++ src/main/java/com/application/billingsystem/entity/PayEntity.java
@@ -1,11 +1,46 @@
 package com.application.billingsystem.entity;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "pay_transactions")
 public class PayEntity {
 
-    private final long id;
-    private final String numberPhone;
-    private final float money;
+    @Id
+    @SequenceGenerator(
+            name = "payload_id_seq",
+            sequenceName = "payload_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "payload_id_seq"
+    )
+    private long id;
+    private String numberPhone;
+    private float money;
+
+    public PayEntity(String numberPhone, float money) {
+        this.numberPhone = numberPhone;
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PayEntity payEntity)) return false;
+        return id == payEntity.id && Float.compare(payEntity.money, money) == 0 && numberPhone.equals(payEntity.numberPhone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, numberPhone, money);
+    }
 }
