dafdfac49fdff3a124c5348cd322d06e3bb3e70e
kirlozavr <kirill.protchenko@mail.ru>
2023-04-16 21:21:54 +1000

    Репозиторий ChangeSubscriberTariff

---
+++ src/main/java/com/application/billingsystem/repositories/ChangeSubscriberTariffRepository.java
@@ -0,0 +1,7 @@
+package com.application.billingsystem.repositories;
+
+import com.application.billingsystem.entity.ChangeSubscriberTariffEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChangeSubscriberTariffRepository extends CrudRepository<ChangeSubscriberTariffEntity, Long> {
+}
