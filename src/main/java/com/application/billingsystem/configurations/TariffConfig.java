755dd04799594e6b4a98e826412f7e5654b3132a
kirlozavr <kirill.protchenko@mail.ru>
2023-04-20 03:59:14 +1000

    Добавляет тарифы в БД

--- src/main/java/com/application/billingsystem/configurations/TariffConfig.java
+++ src/main/java/com/application/billingsystem/configurations/TariffConfig.java
@@ -7,38 +7,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Configuration
 public class TariffConfig {
 
     @Bean
     @Transactional
-    public TariffEntity newTariff(){
-        return new TariffEntity(
-                "06",
-                "Безлимит 300",
-                300.0f,
-                0.0f,
-                1.0f,
-                1.5f,
-                100.0f,
-                "rubles"
-        );
+    public CommandLineRunner commandLine(TariffRepository tariffRepository){
+        return args -> {
+            tariffRepository.save(new TariffEntity(
+                    "06",
+                    "Безлимит 300",
+                    300.0f,
+                    0.0f,
+                    1.0f,
+                    1.5f,
+                    100.0f,
+                    "rubles"
+            ));
+            tariffRepository.save(new TariffEntity(
+                    "03",
+                    "Поминутный",
+                    0,
+                    0.0f,
+                    1.5f,
+                    1.5f,
+                    0.0f,
+                    "rubles"
+            ));
+            tariffRepository.save(new TariffEntity(
+                    "11",
+                    "Обычный",
+                    100.0f,
+                    0.5f,
+                    1.5f,
+                    0.0f,
+                    0.0f,
+                    "rubles"
+            ));
+        };
     }
-
-//    @Bean
-//    @Transactional
-//    CommandLineRunner commandLine(TariffRepository tariffRepository){
-//        return args -> {
-//            new TariffEntity(
-//                    "06",
-//                    "Безлимит 300",
-//                    300.0f,
-//                    0.0f,
-//                    1.0f,
-//                    1.5f,
-//                    100.0f,
-//                    "rubles"
-//            );
-//        };
-//    }
 }
