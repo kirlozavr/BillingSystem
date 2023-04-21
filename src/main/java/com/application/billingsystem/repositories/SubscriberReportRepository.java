4b3b7e39036f409173dd09de9fc7aec6e44bc2d8
kirlozavr <kirill.protchenko@mail.ru>
2023-04-21 17:06:45 +1000

    Репозиторий отчета

---
+++ src/main/java/com/application/billingsystem/repositories/SubscriberReportRepository.java
@@ -0,0 +1,14 @@
+package com.application.billingsystem.repositories;
+
+import com.application.billingsystem.entity.SubscriberReportEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubscriberReportRepository
+        extends JpaRepository<SubscriberReportEntity, Long> {
+
+    Optional<SubscriberReportEntity> findByNumberPhone(String numberPhone);
+}
