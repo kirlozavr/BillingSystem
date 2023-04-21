a22a4da4c5881142eee15ae9bb0d9402e4383db7
kirlozavr <kirill.protchenko@mail.ru>
2023-04-21 17:07:58 +1000

    .

--- src/test/java/com/application/billingsystem/BillingSystemApplicationTests.java
+++ src/test/java/com/application/billingsystem/BillingSystemApplicationTests.java
@@ -3,11 +3,7 @@ package com.application.billingsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 class BillingSystemApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
