0ff00232db5a2afde1417ffacc1af8c8ece19ff3
kirlozavr <kirill.protchenko@mail.ru>
2023-04-21 17:03:52 +1000

    Маппер даты и времени, сделан для удобства парсинга времени и представления их в удобном формате

---
+++ src/main/java/com/application/billingsystem/main/DateMapper.java
@@ -0,0 +1,61 @@
+package com.application.billingsystem.main;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/** Класс для маппинга даты и времени в нужный формат **/
+public class DateMapper {
+
+    private static final String FORMAT_TIME = "HH:mm:ss";
+    private static final String FORMAT_DATE_INPUT = "yyyyMMddHHmmss";
+    private static final String FORMAT_DATE_AND_TIME_OUTPUT = "yyyy-MM-dd HH:mm:ss";
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(FORMAT_TIME);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER_INPUT = DateTimeFormatter.ofPattern(FORMAT_DATE_INPUT);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER_OUTPUT = DateTimeFormatter.ofPattern(FORMAT_DATE_AND_TIME_OUTPUT);
+
+    /** Получить LocalDateTime из текстового формата **/
+    public static LocalDateTime getStringToLocaleDateTime(String dateTime) {
+        return LocalDateTime.parse(dateTime, DATE_TIME_FORMATTER_INPUT);
+    }
+
+    /** Текстовый формат из LocalDateTime **/
+    public static String getLocalDateTimeToString(LocalDateTime localDateTime) {
+        return DATE_TIME_FORMATTER_OUTPUT.format(localDateTime);
+    }
+
+    /** Получить текстовый формат yyyy-MM-dd HH:mm:ss из yyyyMMddHHmmss **/
+    public static String getStringToDateTimeString(String dateTime){
+        return DateMapper.getLocalDateTimeToString(
+                DateMapper.getStringToLocaleDateTime(dateTime)
+        );
+    }
+
+    /** Получить продолжительность звонка в минутах, каждая последующая секунда считается как новая минута **/
+    public static int getDurationToMinute(String startTime, String endTime){
+        final long durationSeconds = DateMapper.getDurationSeconds(startTime, endTime);
+        final int minutes = (int) ((durationSeconds % 3600) / 60);
+        final int seconds = (int) (durationSeconds % 60);
+        return seconds > 0 ? minutes + 1 : minutes;
+    }
+
+    /** Получить продолжительность звонка в текстовом формате **/
+    public static String getDurationToString(String startTime, String endTime) {
+        final long durationSeconds = DateMapper.getDurationSeconds(startTime, endTime);
+
+        final int hours = (int) (durationSeconds / 3600);
+        final int minutes = (int) ((durationSeconds % 3600) / 60);
+        final int seconds = (int) (durationSeconds % 60);
+        LocalTime localTime = LocalTime.of(hours, minutes, seconds);
+        return TIME_FORMATTER.format(localTime);
+    }
+
+    /** Получить общее количество продолжительности звонка в секундах **/
+    private static long getDurationSeconds(String startTime, String endTime) {
+        return Duration.between(
+                DateMapper.getStringToLocaleDateTime(startTime),
+                DateMapper.getStringToLocaleDateTime(endTime)
+        ).toSeconds();
+    }
+}
