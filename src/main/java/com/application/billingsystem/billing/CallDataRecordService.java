53e75b6cb099d1ad92d501594c599da63fda378a
kirlozavr <kirill.protchenko@mail.ru>
2023-04-22 19:18:55 +1000

    CDR сервис для формирования cdr file

--- src/main/java/com/application/billingsystem/billing/CallDataRecordService.java
+++ src/main/java/com/application/billingsystem/billing/CallDataRecordService.java
@@ -1,12 +1,17 @@
 package com.application.billingsystem.billing;
 
 import com.application.billingsystem.dto.SubscriberReportDto;
+import com.application.billingsystem.entity.CallDataRecordEntity;
+import com.application.billingsystem.file_handler.FileWriterHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@Component
+import java.util.List;
+
+@Controller
+@RequestMapping("*/billing")
 public class CallDataRecordService {
 
     private final BillingContract.BRT<SubscriberReportDto> contractBrt;
@@ -16,10 +21,14 @@ public class CallDataRecordService {
         this.contractBrt = contractBrt;
     }
 
-    @RequestMapping(value = "*/billing/run", method = RequestMethod.PATCH)
-    public void run(){
-        String path = null;
-        contractBrt.run(path);
+    @GetMapping("/run")
+    public String run() {
+
+        List<CallDataRecordEntity> list = DataGenerator.generateCdrList(5000);
+        FileWriterHandler.writeCdrFileAndReturnPath(list);
+        contractBrt.run();
+
+        return "Запущена тарификация";
     }
 
 }
