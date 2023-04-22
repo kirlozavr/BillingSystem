33689fa6c70b3c6b4db5fee6ba04295668361874
kirlozavr <kirill.protchenko@mail.ru>
2023-04-22 19:22:12 +1000

    Конфиг для генерации пользователей

---
+++ src/main/java/com/application/billingsystem/configurations/SubscriberConfig.java
@@ -0,0 +1,29 @@
+package com.application.billingsystem.configurations;
+
+import com.application.billingsystem.billing.DataGenerator;
+import com.application.billingsystem.dto.SubscriberCreateDto;
+import com.application.billingsystem.mapping.SubscriberMapper;
+import com.application.billingsystem.repositories.SubscriberRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Configuration
+public class SubscriberConfig {
+
+    @Bean
+    @Transactional
+    public CommandLineRunner commandLineSubscriber(SubscriberRepository repository){
+        return args -> {
+            SubscriberMapper mapper = new SubscriberMapper();
+            List<SubscriberCreateDto> list = DataGenerator.generateSubscribersList(2000);
+            list
+                    .stream()
+                    .map(mapper::getCreateDtoToEntity)
+                    .forEach(repository::save);
+        };
+    }
+}
