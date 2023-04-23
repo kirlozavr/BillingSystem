b697b569abff5407003d74eb386cc3fe17d130f9
kirlozavr <kirill.protchenko@mail.ru>
2023-04-23 21:10:08 +1000

    Класс для проверки корректности введенных данных

---
+++ src/main/java/com/application/billingsystem/utils/ValidationUtils.java
@@ -0,0 +1,39 @@
+package com.application.billingsystem.utils;
+
+import com.application.billingsystem.exceptions.IncorrectArgumentException;
+
+import java.util.regex.Pattern;
+
+/** Класс для проверки корректности введенных данных **/
+public class ValidationUtils {
+
+    public static void checkId(long id){
+        if(id < 0){
+            throw new IncorrectArgumentException("Id не может быть меньше 1");
+        }
+    }
+
+    public static void checkIndex(String index){
+        if(index == null){
+            throw new IncorrectArgumentException("Index не указан");
+        }
+    }
+
+    /** Понятное дело, что код номера бывает разный и соответственно длина номера разная,
+     * плюсом есть спец. символы, но в рамках этого задания сделана упрощенная проверка **/
+    public static void checkNumberPhone(String numberPhone){
+        var regex = "[0-9]";
+        var pattern = Pattern.compile(regex);
+        if(numberPhone.length() != 11){
+            throw new IncorrectArgumentException("Номер телефона не может быть меньше 11 символов");
+        } else if(!pattern.matcher(numberPhone).matches()){
+            throw new IncorrectArgumentException("Номер телефона должен состоять из цифр");
+        }
+    }
+
+    public static void checkPayment(long money){
+        if (money < 1){
+            throw new IncorrectArgumentException("Платеж не может быть меньше 1");
+        }
+    }
+}
