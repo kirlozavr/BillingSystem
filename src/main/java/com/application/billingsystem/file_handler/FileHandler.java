7f93c06a9db5be5dda3231053d0419aaa218673d
kirlozavr <kirill.protchenko@mail.ru>
2023-04-23 21:11:29 +1000

    Абстрактный класс переделал в интерфейс за ненадобностью абстрактного класса

--- src/main/java/com/application/billingsystem/file_handler/FileHandler.java
+++ src/main/java/com/application/billingsystem/file_handler/FileHandler.java
@@ -1,8 +1,19 @@
 package com.application.billingsystem.file_handler;
 
-public abstract class FileHandler {
+import com.application.billingsystem.entity.CallDataRecordEntity;
+import com.application.billingsystem.entity.CallDataRecordPlusEntity;
 
-    protected static final String CDR_FILE_PATH = ".\\src\\main\\resources\\cdr_file\\CDR.txt";
-    protected static final String CDR_PLUS_FILE_PATH = ".\\src\\main\\resources\\cdr_file\\PLUS_CDR.txt";
+import java.util.List;
 
+public interface FileHandler {
+    String CDR_FILE_PATH = ".\\src\\main\\resources\\cdr_file\\CDR.txt";
+    String CDR_PLUS_FILE_PATH = ".\\src\\main\\resources\\cdr_file\\PLUS_CDR.txt";
+    interface Read{
+        List<CallDataRecordEntity> readCDRFileAndReturnListEntity();
+        List<CallDataRecordPlusEntity> readCDRPlusFileAndReturnListEntity();
+    }
+    interface Write{
+        boolean writeCdrFileAndReturnPath(List<CallDataRecordEntity> callDataRecordEntityList);
+        boolean writeCdrPlusFileAndReturnPath(List<CallDataRecordPlusEntity> callDataRecordPlusEntityList);
+    }
 }
