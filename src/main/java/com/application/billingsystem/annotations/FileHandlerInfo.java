6a58e7180c790db7af98d3911b323082c91b0f84
kirlozavr <kirill.protchenko@mail.ru>
2023-04-23 21:10:55 +1000

    Аннотация для информирования о работе файловых обработчиков

---
+++ src/main/java/com/application/billingsystem/annotations/FileHandlerInfo.java
@@ -0,0 +1,12 @@
+package com.application.billingsystem.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FileHandlerInfo {
+    String value() default "";
+}
