60187bba6ea246fe6e40ceb454abea387fefe1ea
kirlozavr <kirill.protchenko@mail.ru>
2023-04-26 12:39:34 +1000

    Добавил маппер

--- src/main/java/com/application/billingsystem/billing/tariffs/Tariff.java
+++ src/main/java/com/application/billingsystem/billing/tariffs/Tariff.java
@@ -2,10 +2,12 @@ package com.application.billingsystem.billing.tariffs;
 
 import com.application.billingsystem.dto.SubscriberReportDto;
 import com.application.billingsystem.entity.CallDataRecordPlusEntity;
+import com.application.billingsystem.mapping.SubscriberReportMapper;
 
 import java.util.List;
 
 public interface Tariff {
 
+    SubscriberReportMapper MAPPER = new SubscriberReportMapper();
     SubscriberReportDto calculate(List<CallDataRecordPlusEntity> entityList);
 }
