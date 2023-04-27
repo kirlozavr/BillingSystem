53c4c6808cea30add9f7d892ff612325b2ff75f1
kirlozavr <kirill.protchenko@mail.ru>
2023-04-27 11:41:02 +1000

    Убрал @Component

--- src/main/java/com/application/billingsystem/billing/tariffs/UnlimitedTariff.java
+++ src/main/java/com/application/billingsystem/billing/tariffs/UnlimitedTariff.java
@@ -5,11 +5,9 @@ import com.application.billingsystem.entity.CallDataRecordPlusEntity;
 import com.application.billingsystem.entity.PayloadEntity;
 import com.application.billingsystem.entity.SubscriberReportEntity;
 import com.application.billingsystem.utils.DateMapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public class UnlimitedTariff implements Tariff {
 
     private static final String TARIFF_INDEX = "06";
