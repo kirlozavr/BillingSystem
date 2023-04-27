c7fac607adfb42ec46f896a7a784e28cb3e60342
kirlozavr <kirill.protchenko@mail.ru>
2023-04-27 11:40:15 +1000

    Убрал @EntityScan и @EnableJpaRepositories

--- src/main/java/com/application/billingsystem/BillingSystemApplication.java
+++ src/main/java/com/application/billingsystem/BillingSystemApplication.java
@@ -3,13 +3,9 @@ package com.application.billingsystem;
 import com.application.billingsystem.billing.CallDataRecordService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.application.billingsystem.entity")
-@EnableJpaRepositories("com.application.billingsystem.repositories")
 public class BillingSystemApplication {
 
     public static void main(String[] args) {
