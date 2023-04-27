ce4941d8d06436956f067ccb46864fad5e518e91
kirlozavr <kirill.protchenko@mail.ru>
2023-04-27 16:19:10 +1000

    Убрал ненужную теперь логику работы с тарифом

--- src/main/java/com/application/billingsystem/billing/HighPerformanceRatingServer.java
+++ src/main/java/com/application/billingsystem/billing/HighPerformanceRatingServer.java
@@ -6,7 +6,6 @@ import com.application.billingsystem.billing.tariffs.TariffHandler;
 import com.application.billingsystem.dto.SubscriberReportDto;
 import com.application.billingsystem.entity.*;
 import com.application.billingsystem.file_handler.FileHandler;
-import com.application.billingsystem.services.TariffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,17 +14,14 @@ import java.util.*;
 @Component
 public class HighPerformanceRatingServer implements BillingContract.HRS {
 
-    private final TariffService tariffService;
     private final BillingContract.BRT<SubscriberReportDto> contractBrt;
     private final FileHandler.Read fileRead;
 
     @Autowired
     public HighPerformanceRatingServer(
-            TariffService tariffService,
             BillingContract.BRT<SubscriberReportDto> contractBrt,
             FileHandler.Read fileRead
     ) {
-        this.tariffService = tariffService;
         this.contractBrt = contractBrt;
         this.fileRead = fileRead;
     }
@@ -40,27 +36,14 @@ public class HighPerformanceRatingServer implements BillingContract.HRS {
      * Метод сортирует CDR+ по абонентам
      **/
     private void sortingSubscriberCalls() {
-        /** Кэш тарифов, чтобы лишний раз не обращаться в БД, где ключ - индекс тарифа **/
-        final Map<String, TariffEntity> tariffEntityHashMap = new HashMap<>();
         /** Список CDR+ сущностей, где ключ - номер телефона абонента **/
         final Map<String, DataListEntity<CallDataRecordPlusEntity>> cdrPlusHashMap = new HashMap<>();
         /** Читаем CDR+ и получаем информацию в списке **/
         final List<CallDataRecordPlusEntity> callDataRecordPlusEntityList =
                 fileRead.readCDRPlusFileAndReturnListEntity();
 
-        TariffEntity tariffEntity;
-
         if (!callDataRecordPlusEntityList.isEmpty()) {
             for (CallDataRecordPlusEntity entity : callDataRecordPlusEntityList) {
-
-                /** Те тарифы, которых нет в локальном кэше, добавляются туда,
-                 * же тариф уже есть, то просто берем из кэша **/
-                if (tariffEntityHashMap.containsKey(entity.getTariffIndex())) {
-                    tariffEntity = tariffEntityHashMap.get(entity.getTariffIndex());
-                } else {
-                    tariffEntity = tariffService.getByIndex(entity.getTariffIndex());
-                    tariffEntityHashMap.put(tariffEntity.getTariffIndex(), tariffEntity);
-                }
 
                 /** Тут CallDataRecordPlusEntity групируются по номеру телефона и заносятся в мапу **/
                 if (cdrPlusHashMap.containsKey(entity.getNumberPhone())) {
@@ -75,7 +58,7 @@ public class HighPerformanceRatingServer implements BillingContract.HRS {
                 }
             }
         }
-        generateSubscriberReports(cdrPlusHashMap, tariffEntityHashMap);
+        generateSubscriberReports(cdrPlusHashMap);
     }
 
     /**
@@ -83,8 +66,7 @@ public class HighPerformanceRatingServer implements BillingContract.HRS {
      * который содержит результат тарификации и отправляет его обратно в BRT
      **/
     private void generateSubscriberReports(
-            Map<String, DataListEntity<CallDataRecordPlusEntity>> cdrPlus,
-            Map<String, TariffEntity> tariffs
+            Map<String, DataListEntity<CallDataRecordPlusEntity>> cdrPlus
     ) {
         for (Map.Entry<String, DataListEntity<CallDataRecordPlusEntity>> entryMap : cdrPlus.entrySet()) {
             Tariff tariff = TariffHandler
@@ -100,7 +82,6 @@ public class HighPerformanceRatingServer implements BillingContract.HRS {
 
     /**
      * Метод формирует отчет, исходя из тарифа абонента.
-     * Переделал логику расчета, но остальной код пока не переделывал.
      **/
     private SubscriberReportDto generateSubscriberReportByTariff(
             List<CallDataRecordPlusEntity> entityList,
